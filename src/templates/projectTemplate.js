8d9ffa65c6c4566527070879cfda3a3f3584c442
Blakemcw <blakecmcwilliam@gmail.com>
2020-06-14 13:17:07 -0700

    Refactor code add styling for project posts

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -3,14 +3,34 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 export default function ProjectTemplate({ data }) {
+  // ===========================================================================
+  // GraphQL Data
+  // ===========================================================================
+  
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
+
+  // ===========================================================================
+  // Styling
+  // ===========================================================================
+
+  let contentStyle = {
+    color: `#484848`,
+    textDecoration: `none`,
+    maxWidth: `900px`,
+    margin: `0 auto`
+  }
+
+  // ===========================================================================
+  // Render
+  // ===========================================================================
+
   return (
     <Layout>
-      <div className="project-container">
+      <div className="project-container" style={contentStyle}>
         <div className="project">
           <h1>{frontmatter.title}</h1>
-          <h6>{frontmatter.date}</h6>
+          <h6>{frontmatter.startDate} - {frontmatter.endDate}</h6>
           <div
             className="project-content"
             dangerouslySetInnerHTML={{ __html: html }}
@@ -27,7 +47,8 @@ export const pageQuery = graphql`
       html
       frontmatter {
         slug
-        date(formatString: "MMMM DD, YYYY")
+        startDate(formatString: "MMMM DD, YYYY")
+        endDate(formatString: "MMMM DD, YYYY")
         title
       }
     }
